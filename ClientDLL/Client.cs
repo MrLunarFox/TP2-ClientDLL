238154ce0f581672226d821e0ddce9b790505fb2
HIS <hisjosuapro@gmail.com>
2023-02-09 10:12:08 +0100

    Tp2 - ClientDLL V.1

--- ClientDLL/Client.cs
+++ ClientDLL/Client.cs
@@ -1,7 +1,28 @@
-﻿namespace ClientDLL
+﻿using System.Runtime.CompilerServices;
+
+namespace ClientDLL
 {
     public class Client
     {
+        private int num;
+        private string nom;
+        private float ca;
+        private int cat;
 
+        public int Num { get => num; }
+        public string Nom { get => nom; }
+        public float Ca { get => ca; }
+        public int Cat { get => cat; }
+
+        public Client(int numC, string nomC, float caC, int catC)
+        {
+            this.num = numC;
+            this.nom = nomC;
+            this.ca = caC;
+            if (catC >= 1 && catC <= 3)
+            {
+                this.cat = catC;
+            }
+        }
     }
 }
